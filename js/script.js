fb4888fbaf680c488de082005925397cda9a4c10
ericVargas <batman@Erics-MacBook-Pro-2.local>
2018-09-09 02:55:06 -0400

    created function for preset steps and added more comments

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Have text box appear for additional instructions
 function timeSteps(){
     let am = document.querySelector("#am");
     let pm = document.querySelector("#pm");
@@ -23,5 +24,40 @@ function timeSteps(){
         console.log('None checked');
         morning.style.display = "none";
         evening.style.display = "none";
+    }
+}
+
+
+// Display preset instructions for prescription
+function productSelect(){
+    let dif = document.querySelector("#dif");
+    let cli = document.querySelector("#cli");
+    let mup = document.querySelector("#mup");
+    let duo = document.querySelector("#duo");
+    let bpw = document.querySelector("#bpw");
+    let text = document.querySelector("#instruct");
+    
+    if (dif.selected == true){
+        console.log('Dif selected');
+        text.innerHTML = "Differin 0.1% Gel";
+        
+    } else if (cli.selected == true){
+        console.log('Cli selected');
+        text.innerHTML = "Clindamycin Lotion";
+        
+    } else if (mup.selected == true){
+        console.log('Mup selected');
+        text.innerHTML = "Mupirocin";
+        
+    } else if (duo.selected == true){
+        console.log('Duo selected');
+        text.innerHTML = "Duofilm 17%";
+        
+    } else if (bpw.selected == true){
+        console.log('bpw selected');
+        text.innerHTML = "Benzoyl Peroxide Wash";
+        
+    } else {
+        console.log('None selected');
     }
 }
